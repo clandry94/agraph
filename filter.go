6545a3a8c3a083cd814fb364d49099f9136a61bc
Conor Landry <clandry94@ufl.edu>
2017-09-13 03:22:43 -0400

    Add filter foundations

--- filter.go
+++ filter.go
@@ -1,9 +1,27 @@
 package agraph
 
 type FilterGraph struct {
+	Nodes map[string]Node
+}
+
+func (g FilterGraph) Insert(node *Node) error {
+	return nil
+}
+
+func (g FilterGraph) Remove(node *Node) error {
+	return nil
+}
+
+func (g FilterGraph) Process(data []uint8) {
 
 }
 
 type Node struct {
+	Source chan []byte
+	Sink   chan []byte
+	Next chan []byte
+}
 
+func (n Node) process() error {
+	return nil
 }
