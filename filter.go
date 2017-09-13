f19e1ce52e27f66392d8922aab4305b9017d2e34
Conor Landry <clandry94@ufl.edu>
2017-09-13 03:38:08 -0400

    Add filter creation boilerplate

--- filter.go
+++ filter.go
@@ -1,5 +1,7 @@
 package agraph
 
+import "fmt"
+
 type FilterGraph struct {
 	Nodes map[string]Node
 }
@@ -15,13 +17,32 @@ func (g FilterGraph) Remove(node *Node) error {
 func (g FilterGraph) Process(data []uint8) {
 
 }
+type Node interface{
+	process()
+}
 
-type Node struct {
+type NopFilter struct {
 	Source chan []byte
 	Sink   chan []byte
 	Next chan []byte
 }
 
-func (n Node) process() error {
+func (n NopFilter) process() error {
+	for {
+		select {
+			case data := <-n.Source:
+				fmt.Println("found data")
+				var filteredData, err = n.do(&data)
+
+				if err != nil {
+					panic("Could not filter!")
+				}
+				n.Sink <- filteredData
+		}
+	}
 	return nil
+}
+
+func (n NopFilter) do(data *[]byte) ([]byte, error) {
+	return []byte("Hello, world!"), nil
 }
