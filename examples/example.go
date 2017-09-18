42012caf24dc9a254679f74aaea6fd88ceb4c06d
Conor Landry <clandry94@ufl.edu>
2017-09-17 23:11:46 -0400

    Added RIFF chunk parser

--- examples/example.go
+++ examples/example.go
@@ -6,18 +6,19 @@ import (
 )
 
 func main() {
-	ar, err := agraph.New("test.wav")
+	ar, err := agraph.New("ringbackA.wav")
 	if err != nil {
 		fmt.Println(err)
 	}
 
 	fmt.Println(ar.Meta)
 
+	/*
 	firstNode, _ := agraph.NewNode(agraph.NopFilter)
 	secondNode, _ := agraph.NewNode(agraph.VolumeFilter)
 	thirdNode, _ := agraph.NewNode(agraph.NopFilter)
 
 	firstNode.SetSink(secondNode.Source())
 	secondNode.SetSink(thirdNode.Source())
-
+	*/
 }
--- reader.go
+++ reader.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"bytes"
+	"encoding/binary"
 )
 
 /*
@@ -60,12 +61,16 @@ const (
 //		|		        |
 //		|               |
 //		 ---------------
+
+// Riff Chunk
 type Riff struct {
-	ID         []byte
-	Size       uint32
-	FormatType []byte
+	ChunkID         []byte
+	ChunkSize       uint32
+	Format []byte
 }
 
+
+// Fmt Chunk
 type Fmt struct {
 	ID   []byte
 	Size uint32
@@ -75,12 +80,13 @@ type Fmt struct {
 type WavFmtData struct {
 	AudioFormat   uint16
 	NumChannels   uint16
-	SampleRate    uint32
+	SampleRate    uint32 /// per second
 	ByteRate      uint32
-	BlockAlign    uint16 // data block size in bytes
+	BlockAlign    uint16 // data block size in bytes aka numChannels * BitsPerSample /8
 	BitsPerSample uint16
 }
 
+// Data Chunk
 type Data struct {
 	ID   []byte
 	Size uint32
@@ -107,35 +113,103 @@ type WaveReader struct {
 	Data *Data
 
 	dataSource int64
-	NumSamples uint32
+	SampleCount uint32
 	ReadSample int32
 	SampleTime int
 }
 
-func NewWaveReader(fp *os.File) (reader *WaveReader, err error) {
+func NewWaveReader(fp *os.File) (*WaveReader, error) {
 	defer fp.Close()
 	fStat, err := fp.Stat()
 	if err != nil {
-		return reader, err
+		return &WaveReader{}, err
 	}
 
 	if fStat.Size() > maxFileSize {
-		return reader, fmt.Errorf("File size (%v bytes) is too large", fStat.Size())
+		return &WaveReader{}, fmt.Errorf("File size (%v bytes) is too large", fStat.Size())
 	}
 
 	data, err := ioutil.ReadAll(fp)
 	if err != nil {
-		return reader, err
+		return &WaveReader{}, err
 	}
 
+	fmt.Printf("File Size: %v\n", fStat.Size())
+	reader := new(WaveReader)
 	reader.size = fStat.Size()
 	reader.in = bytes.NewReader(data)
 
 	/*
-		TODO: Need to parse riff, fmt, list, and data chunks here
+		TODO: Need to fmt, list, and data chunks here
 	 */
+	 err = reader.riffChunkParser()
+	 if err != nil {
+	 	return reader, err
+	 }
+
+
+	//reader.SampleCount = reader.Data.Size / uint32(reader.Fmt.Data.BlockAlign)
+	//reader.SampleTime = int(reader.SampleCount / reader.Fmt.Data.SampleRate)
 
 	return reader, nil
+}
+
+/*
+	TODO: Clean these parsers up
+ */
+func (r WaveReader) riffChunkParser() error {
+	chunkId := make([]byte, 4)
+
+	// Read the RIFF token from the
+	err := binary.Read(r.in, binary.BigEndian, chunkId)
+	fmt.Printf("Chunk ID in bytes: %v\n", chunkId)
+	fmt.Printf("Chunk ID as string: %v\n", string(chunkId))
+	if err != nil{
+		return err
+	}
+
+	if string(chunkId[:]) != riffChunkToken {
+		return fmt.Errorf("Not a WAVE file. File is of type %s", string(chunkId[:]))
+	}
+
+	// Grab the 16 bit fmt chunk size
+	// This is the size of the
+	// entire file in bytes minus 8 bytes for the
+	// two fields not included in this count:
+	// ChunkID and ChunkSize.
+	chunkSizeBytes := make([]byte, 4)
+	err = binary.Read(r.in, binary.LittleEndian, chunkSizeBytes)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Chunk Size in Bytes: %v\n", chunkSizeBytes)
+	chunkSize := binary.LittleEndian.Uint32(chunkSizeBytes)
+	fmt.Printf("Chunk size as decimal %v\n", chunkSize)
+
+	if chunkSize != uint32(r.size)-8 {
+		return fmt.Errorf("RIFF Chunk Size %v must == file size-8 bytes %v", chunkSize, r.size-8 )
+	}
+
+	format := make([]byte, 4)
+	err = binary.Read(r.in, binary.BigEndian, format)
+	if err != nil {
+		return err
+	}
+
+	if string(format[:]) != wavFormatToken {
+		return fmt.Errorf("File is not a WAVE file. It is %s", string(format[:]))
+	}
+
+	riff := Riff{
+		ChunkID: chunkId,
+		ChunkSize: chunkSize,
+		Format: format,
+	}
+
+	r.Riff = &riff
+
+	return nil
 }
 
 //func WavFormatReader(r io.Reader, n int64) io.Reader { return &WaveReader{r, n }}
