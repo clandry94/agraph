51fbc74cbfaa2f023ef4de12a3c68f54e926f49a
Joe Pena <josephpena@fb.com>
2017-09-20 20:20:17 -0700

    Add fmt chunk parsing for WAVE files and appropriate tests

--- reader_test.go
+++ reader_test.go
@@ -19,6 +19,14 @@ func TestNewWaveReader(t *testing.T) {
 	expectedID := "RIFF"
 	expectedSize := uint32(160038) // take off the 8 bits??
 	expectedFormat := "WAVE"
+	expectedSubChunk1ID := "fmt "
+	expectedSubChunk1Size := uint32(16)
+	expectedAudioFormat := uint16(1)
+	expectedNumChannels := uint16(1)
+	expectedSampleRate := uint32(8000)
+	expectedByteRate := uint32(16000)
+	expectedBlockAlign := uint16(2)
+	expectedBitsPerSamp := uint16(16)
 
 	if string(reader.Riff.ChunkID) != expectedID {
 		t.Errorf("Actual chunk ID %v is not equal to expected chunk ID %v", expectedID, reader.Riff.ChunkID)
@@ -29,5 +37,37 @@ func TestNewWaveReader(t *testing.T) {
 	}
 	if string(reader.Riff.Format) != expectedFormat {
 		t.Errorf("Actual format %v is not equal to expected formmat %v", expectedFormat, reader.Riff.Format)
+	}
+
+	if string(reader.Fmt.ID) != expectedSubChunk1ID {
+		t.Errorf("Actual subChunk1ID %v is not equal to expected subChunk1 ID %v", string(reader.Fmt.ID), expectedSubChunk1ID)
+	}
+
+	if reader.Fmt.Size != expectedSubChunk1Size {
+		t.Errorf("Actual subChunk1Size %v is not equal to expected subchunk1Size %v", reader.Fmt.Size, expectedSubChunk1Size)
+	}
+
+	if reader.Fmt.Data.AudioFormat != expectedAudioFormat {
+		t.Errorf("Actual audio-format %v is not equal to expected audio-format %v", reader.Fmt.Data.AudioFormat, expectedAudioFormat)
+	}
+
+	if reader.Fmt.Data.NumChannels != expectedNumChannels {
+		t.Errorf("Actual num-channels %v is not equal to expected num-channels %v", reader.Fmt.Data.NumChannels, expectedNumChannels)
+	}
+
+	if reader.Fmt.Data.SampleRate != expectedSampleRate {
+		t.Errorf("Actual sample-rate %v is not equal to expected sample-rate %v", reader.Fmt.Data.SampleRate, expectedSampleRate)
+	}
+
+	if reader.Fmt.Data.ByteRate != expectedByteRate {
+		t.Errorf("Actual byte-rate %v is not equal to expected byte-rate %v", reader.Fmt.Data.ByteRate, expectedByteRate)
+	}
+
+	if reader.Fmt.Data.BlockAlign != expectedBlockAlign {
+		t.Errorf("Actual block-align %v is not equal to expected block-align %v", reader.Fmt.Data.BlockAlign, expectedBlockAlign)
+	}
+
+	if reader.Fmt.Data.BitsPerSample != expectedBitsPerSamp {
+		t.Errorf("Actual bits-per-sample %v is not equal to expected bits-per-sample %v", reader.Fmt.Data.BitsPerSample, expectedBitsPerSamp)
 	}
 }
